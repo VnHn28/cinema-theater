9fb006f099e1545c61c885457575d488b016c76f
Vincent Handoyo <vincenthan28@gmail.com>
2026-02-03 04:08:40 +0800

    fix js bug

--- js/script.js
+++ js/script.js
@@ -1,65 +1,63 @@
 $(document).ready(function(){
-    let arrMovie = [];
-    let movie;
-    let objSeat;
-    let arrMovieSeating = [];
-    let seatingIndex=0;
-    let whichmovieSeats;
-    let seatSelection;
-    let numofseatstoSelect;
+    const arrMovie = [];
+    const arrMovieSeating = [];
+    let seatingIndex = 0;
+    let whichMovieSeats; // The index of the movie whose seats are being displayed
+    let seatSelection; // How many seats are left to pick
+    let numofseatstoSelect; // The original number of seats the user wanted
+    let arrSelectedseat = []; // Holds currently picked seats {row, col}
 
-    //if(seatSelection<=0){
-        $("#bottom").hide();
-    //}
+    $("#bottom").hide();
 
     $("#btnAddMovie").click(function(){
         let duplicateSchedule = false;
+
+        if ($("#txtTitle").val().trim() === "") {
+            alert("Please enter a movie title!");
+            return;
+        }
         if(arrMovie.length){
-            for (var i = 0; i < arrMovie.length; i++) {
-                let thisMovie = arrMovie[i];
-                let movie_time=$("#cmbTime option:selected").text();
-                let movie_studio=$("#cmbStudio option:selected").val();
-                if(thisMovie.time==movie_time && thisMovie.studio==movie_studio){
+            const newMovieTime = $("#cmbTime option:selected").text();
+            const newMovieStudio = $("#cmbStudio option:selected").val();
+            for (const movie of arrMovie) {
+                if(movie.time === newMovieTime && movie.studio === newMovieStudio){
                     duplicateSchedule = true;
                     break;
                 }
             }
         }
+
         if (duplicateSchedule){
             alert("Unable to set this schedule for this cinema!");
         } else {
-            movie = new Object();
-            movie.title=$("#txtTitle").val();
-            movie.time=$("#cmbTime option:selected").text();
-            movie.studio=$("#cmbStudio option:selected").val();
-            movie.index=seatingIndex;
+            const movie = {
+                title: $("#txtTitle").val(),
+                time: $("#cmbTime option:selected").text(),
+                studio: $("#cmbStudio option:selected").val(),
+                index: seatingIndex
+            };
 
             arrMovie.push(movie);
             seatingIndex++;
 
             generateSeats();
-
             displaySchedule();
         }
-    })
+    });
 
     function generateSeats(){
-        let arrRow=[];
-        //$("#seats").innerHTML=t;
-        for (var i = 0; i < 15; i++) {
-            var arrCollumn = [];
-            row=String.fromCharCode(i+65);
-            for (var j = 0; j < 18; j++) {
-                //console.log(row+j)
-                objSeat = new Object();
-                if(j<10){
-                    objSeat.id = row+'0'+j;
-                } else{
-                    objSeat.id = row+j;
-                }
-                objSeat.status = "non";
-                objSeat.row = row;
-                objSeat.col = j;
+        const arrRow = [];
+        for (let i = 0; i < 15; i++) {
+            const arrCollumn = [];
+            const row = String.fromCharCode(i + 65);
+            for (let j = 0; j < 18; j++) {
+                const seatId = row + (j < 10 ? '0' + j : j);
+                const objSeat = {
+                    id: seatId,
+                    status: "non", // 'non', 'picked', 'booked'
+                    row: i,
+                    col: j
+                };
                 arrCollumn.push(objSeat);
             }
             arrRow.push(arrCollumn);
@@ -67,155 +65,139 @@ $(document).ready(function(){
         arrMovieSeating.push(arrRow);
     }
 
-    $("#cmbSchedule").change(function(){
-        displaySchedule();
-    })
+    $("#cmbSchedule").change(displaySchedule);
 
     function displaySchedule(){
-        var thisStudio = $("#cmbSchedule option:selected").val();
-        //alert(thisStudio)
+        const thisStudio = $("#cmbSchedule option:selected").val();
+        const timeTable = $("#timeTable");
         $("#timeTable").empty();
-        for (var i = 0; i < arrMovie.length; i++) {
-            let thisMovie = arrMovie[i];
-            if(thisMovie.studio==thisStudio){
-                //alert("in");
-                var newSchedule=document.createElement("div");
-                newSchedule.innerHTML = thisMovie.time+' _ '+thisMovie.title;
-                newSchedule.value = thisMovie.index;
-                newSchedule.className = 'movieSchedule';
-                let loc = $("#timeTable");
-                loc.append(newSchedule);
+        for (const movie of arrMovie) {
+            if(movie.studio === thisStudio){
+                const newSchedule = $("<div>", {
+                    class: 'movieSchedule',
+                    text: `${movie.time} _ ${movie.title}`,
+                    'data-index': movie.index
+                });
+                timeTable.append(newSchedule);
             }
         }
     }
 
     $("body").on("click", ".movieSchedule", function(){
-        whichmovieSeats = $(this).val();
-        //alert(whichmovieSeats);
+        whichMovieSeats = $(this).data('index');
         displaySeats();
-    })
+    });
 
     function displaySeats(){
-        //alert(arrMovieSeating.length);
-        var stairs = 0;
-        var splicechairArr = 0
-        let thisMovie = whichmovieSeats;
-        //alert(thisMovie);
-        let movieSeating = arrMovieSeating[thisMovie];
-        $("#seats").empty();
-        for (var i = 0; i < 15; i++) {
-            for (var j = 0; j < 20; j++) {
-                var seat = document.createElement("div");
-                if (j==4||j==15) {
-                    stairs+=1;
-                    seat.className = "stair";
+        if (typeof whichMovieSeats === 'undefined') return;
+
+        const movieSeating = arrMovieSeating[whichMovieSeats];
+        const seatsContainer = $("#seats").empty();
+
+        for (let i = 0; i < 15; i++) {
+            let stairs = 0;
+            for (let j = 0; j < 20; j++) {
+                if (j === 4 || j === 15) {
+                    seatsContainer.append($("<div>", { class: "stair" }));
+                    stairs++;
                 } else {
-                    seat.innerHTML = movieSeating[i][j-stairs].id;
-                    seat.value = movieSeating[i][j-stairs].status;
-                    //console.log(seat.value);
-                    seat.row = movieSeating[i][j-stairs].row;
-                    //console.log(seat.row);
-                    seat.col = movieSeating[i][j-stairs].col;
-                    //console.log(seat.col);
-                    seat.mov = whichmovieSeats;
-                    //console.log(seat.mov);
-                    seat.arr = splicechairArr;
+                    const seatData = movieSeating[i][j - stairs];
+                    const seat = $("<div>", {
+                        text: seatData.id,
+                        'data-status': seatData.status,
+                        'data-row': seatData.row,
+                        'data-col': seatData.col
+                    });
 
-                    if(seat.value=="non"){
-                        seat.className = "availableSeat";
-                    } else if(seat.value=="booked"){
-                        seat.className="bookedSeat";
+                    if(seatData.status === "non"){
+                        seat.addClass("availableSeat");
+                    } else if(seatData.status === "booked"){
+                        seat.addClass("bookedSeat");
                     }
-                    splicechairArr++;
+                    seatsContainer.append(seat);
                 }
-
-                let loc = $("#seats");
-                loc.append(seat);
             }
-            stairs=0;
         }
     }
 
     $("#btnSelect").click(function(){
-        seatSelection=$("#numSeats").val();
-        numofseatstoSelect=seatSelection;
+        const numSeats = parseInt($("#numSeats").val(), 10);
+        if (isNaN(numSeats) || numSeats <= 0) {
+            alert("Please enter a valid number of seats.");
+            return;
+        }
+
+        seatSelection = numSeats;
+        numofseatstoSelect = numSeats;
+        arrSelectedseat = []; // Reset previous selections
 
         $("#bottom").show();
-        $("#seatLeft").html("Seats to pick: "+seatSelection);
-        alert("Seats to pick: "+seatSelection);
-        mTix();
+        $("#seatLeft").text("Seats to pick: " + seatSelection);
+    });
 
-    })
+    // --- Seat Interaction Event Handlers ---
 
-    function mTix(){
-        let thisMovie;
-        let arrSelectedseat = [];
+    $("body").on("mouseenter", ".availableSeat", function(){
+        $(this).fadeTo("fast", 0.8);
+    }).on("mouseleave", ".availableSeat", function(){
+        $(this).fadeTo("fast", 1);
+    });
 
-        if(seatSelection>-1){
-            $("body").on("mouseenter", ".availableSeat", function(){
-                $(this).fadeTo("fast",0.8);
-            });
-            $("body").on("mouseleave", ".availableSeat", function(){
-                $(this).fadeTo("fast", 1);
-            });
+    $("body").on("click", ".availableSeat", function(){
+        if (typeof seatSelection === 'undefined' || numofseatstoSelect <= 0) {
+            alert("Please first select the number of seats you want to book.");
+            return;
+        }
 
-            if(seatSelection>0){ 
-                $("body").on("click", ".availableSeat", function(){
-                    thisMovie = $(this).prop('mov');
+        const seat = $(this);
+        const seatStatus = seat.data('status');
+        const seatRow = seat.data('row');
+        const seatCol = seat.data('col');
 
-                    if($(this).prop('value')=='non'){
-                        $(this).css({"background-color":'#fec700'});
-                        $(this).prop('value', 'picked');
-                        
-                        objTicket = new Object();
-                        objTicket.index = $(this).prop('arr');
-                        objTicket.row = $(this).prop('row');;
-                        objTicket.col = $(this).prop('col');
-                        arrSelectedseat.push(objTicket);
-
-                        // alert(arrSelectedseat.length);
-                        seatSelection--;
-                         alert(seatSelection);
-                    } else if($(this).prop('value')=="picked"){
-                        console.log("clicked");
-                        for (var i = 0; i < arrSelectedseat.length; i++) {
-                            if(arrSelectedseat[i].index==$(this).prop('arr')){
-                                arrSelectedseat.splice(i,1);
-                            }
-                        }
-                        $(this).css({"background-color":'grey'});
-                        $(this).prop('value', 'non');
-                        seatSelection++;
-                        // alert(seatSelection);
-                    }
-                    //alert($(this).val());
-                    
-                });
-            } else if(seatSelection==0){
-                $("body").on("click", ".availableSeat", function(){
-                    alert("Currently already "+numofseatstoSelect+" seat(s) are selected!")
-                });
-            } 
-
-            $("#btnBook").click(function(){
-                for (var i = 0; i < arrSelectedseat.length; i++) {
-                    let thisRow = arrSelectedseat[i].row;
-                    //alert(thisRow);
-                    let thisCol = arrSelectedseat[i].col;
-                    //alert(thisCol);
-                    let movieSeating = arrMovieSeating[thisMovie];
-
-                    movieSeating[thisRow][thisCol].status = "booked";
-                }
+        if (seatStatus === 'picked') {
+            // DESELECT a seat
+            seat.css({"background-color": 'grey'}).data('status', 'non');
+            seatSelection++;
+            arrSelectedseat = arrSelectedseat.filter(s => !(s.row === seatRow && s.col === seatCol));
+        } else if (seatStatus === 'non') {
+            // SELECT a seat
+            if (seatSelection > 0) {
+                seat.css({"background-color": '#fec700'}).data('status', 'picked');
                 seatSelection--;
-                $("#bottom").hide();
-                displaySeats();
-            });
+                arrSelectedseat.push({ row: seatRow, col: seatCol });
+            } else {
+                alert(`You have already selected ${numofseatstoSelect} seat(s).`);
+            }
+        }
+        $("#seatLeft").text("Seats to pick: " + seatSelection);
+    });
 
-            $("body").on("click", ".bookedSeat", function(){
-                       alert("This seat is already booked!");
-            });
-        } 
-    }
+    $("#btnBook").click(function(){
+        if (seatSelection > 0) {
+            alert(`Please select ${seatSelection} more seat(s) to continue.`);
+            return;
+        }
+        if (arrSelectedseat.length === 0) {
+            alert("No seats were selected to book.");
+            return;
+        }
 
-})
+        const movieSeating = arrMovieSeating[whichMovieSeats];
+        for (const ticket of arrSelectedseat) {
+            const thisRow = ticket.row;
+            const thisCol = ticket.col;
+            movieSeating[thisRow][thisCol].status = "booked";
+        }
+
+        // Reset state and UI
+        seatSelection = undefined;
+        numofseatstoSelect = 0;
+        arrSelectedseat = [];
+        $("#bottom").hide();
+        displaySeats();
+    });
+
+    $("body").on("click", ".bookedSeat", function(){
+        alert("This seat is already booked!");
+    });
+});
